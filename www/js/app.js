841bf567c524125cd24010ec6d6704da1589ca32
Roger Smith <rogerlsmith@live.com>
2014-10-24 22:43:34 -0400

    clean up JS & added bower
    may need to rethink the installation of bower....

--- www/js/app.js
+++ www/js/app.js
@@ -1,33 +1,59 @@
+//
+// Author: Roger L. Smith
+// Date: October 2014
+//
 
-var audioFile = null;
-var filePath = "";
-var fileName = "";
 
-$(function()
+//
+// global
+//
+var audioFile   = null;
+var filePath    = "";
+var fileName    = "";
+
+
+//
+// document ready
+//
+$( function ( )
 {
 
-
+    //
+    // start recording on click
+    //
     $( "#start" ).click ( function ( e )
     {
-    	navigator.device.capture.captureAudio ( captureSuccess, captureError, { limit:2 } );
+    	navigator.device.capture.captureAudio ( captureSuccess, 
+                                                    captureError, 
+                                                    { limit:2 }
+                                            );
     });
 
 
+    //
+    // publish on click
+    //
     $( "#publish" ).click ( function ( e )
     {
      	uploadFile ( audioFile );
     });
 
+    //
+    // browse on click
+    //
     $( "#browse" ).click ( function ( e )
     {
-        var ref = window.open('http://rogerlsmith.net/concept/browse.php', '_self', 'location=no');
+        var ref = window.open ( 'http://rogerlsmith.net/concept/browse.php', 
+                                    '_self', 
+                                    'location=no' );
     });
 
 });
 
 
-
+//
 // capture success callback
+//
 var captureSuccess = function ( mediaFiles ) 
 {
 
@@ -39,15 +65,17 @@ var captureSuccess = function ( mediaFiles )
 };
 
 
-
+//
 // capture error callback
+//
 var captureError = function ( error )
 {
     navigator.notification.alert ( 'Error code: ' + error.code, null, 'Capture Error' );
 };
 
-
+//
 // Upload file to server
+//
 function uploadFile ( ) 
 {
     var ft = new FileTransfer ( ),
