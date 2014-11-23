3a1b9686d864bf2c2394e1bd209aa3df893395f6
Roger Smith <rogerlsmith@live.com>
2014-11-22 23:43:38 -0500

    ajaxed login

--- www/js/index.js
+++ www/js/index.js
@@ -87,6 +87,34 @@ var app =
                                         'location=no' );
         });
 
+
+        //
+        // login on click
+        //
+        $( "#loginForm" ).on ( "submit", function ( e )
+        {
+
+            //disable the button so we can't resubmit while we wait
+            $("#submitButton",this).attr("disabled","disabled");
+
+            var u = $("#username", this).val();
+            var p = $("#password", this).val();
+
+            if ( u != '' && p != '' )
+            {
+                $.post( "http://rogerlsmith.net/concept/bower_components/bootstrap/mobile/user.php?method=login&returnformat=json", { username: u, password:p }, function ( res )
+                {
+                    if(res == true) {
+                        $.mobile.changePage( "success.html" );
+                    } else {
+                        navigator.notification.alert( "Your login failed", function ( ) { } );
+                    }
+                    $( "#submitButton" ).removeAttr( "disabled" );
+                },"json");
+            }
+            return false;
+        });
+
     },
 
 
